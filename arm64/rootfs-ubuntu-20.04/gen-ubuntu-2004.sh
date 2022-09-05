2a0afc8906e3a86ab2c4a6f7fb21696e6be6a784
qsfix-nagaraja <nagarajat@cabcsgroup-india.com>
2022-09-06 00:50:21 +0900

    Added some additional packages for npm installation

--- arm64/rootfs-ubuntu-20.04/gen-ubuntu-2004.sh
+++ arm64/rootfs-ubuntu-20.04/gen-ubuntu-2004.sh
@@ -3,7 +3,8 @@
 update-binfmts --enable qemu-aarch64
 
 mmdebstrap --architectures=arm64 --include=\
-"debian-ports-archive-keyring,locales,wget,curl,openssh-client,openssh-server,sudo,file,libcurl4-gnutls-dev,libjansson-dev,rsync,ntpdate,usbutils,pciutils,net-tools,vim,nodejs,npm" \
+"debian-ports-archive-keyring,locales,wget,curl,openssh-client,openssh-server,sudo,file,libcurl4-gnutls-dev,libjansson-dev,rsync,ntpdate,usbutils,pciutils,net-tools,vim,nodejs-dev,node-gyp,libssl1.0-dev
+,nodejs,npm" \
   focal /tmp/arm64-rootfs \
   "deb http://ports.ubuntu.com/ubuntu-ports/ focal main universe" \
   "deb http://ports.ubuntu.com/ubuntu-ports/ focal-updates main universe" \
