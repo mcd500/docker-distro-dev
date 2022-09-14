36544dbf1bb892b3b61b113cf2bd1bc15c9720c3
qsfix-nagaraja <nagarajat@cabcsgroup-india.com>
2022-09-14 12:44:53 +0900

    Added nameserver here and also removed npm installation

--- arm64/rootfs-ubuntu-20.04/gen-ubuntu-2004.sh
+++ arm64/rootfs-ubuntu-20.04/gen-ubuntu-2004.sh
@@ -36,6 +36,7 @@ sed -i 's/^[#]\?Storage=auto$/Storage=volatile/' /tmp/arm64-rootfs/etc/systemd/j
 sed -i 's/^[#]\?PermitRootLogin[ \t].*$/PermitRootLogin yes/' /tmp/arm64-rootfs/etc/ssh/sshd_config
 chroot /tmp/arm64-rootfs /bin/bash -c 'chmod 644 /etc/ssh/sshd_config'
 echo 'unset HISTFILE 2>/dev/null' >>/tmp/arm64-rootfs/root/.bashrc
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 chroot /tmp/arm64-rootfs /bin/bash -c 'systemctl disable rsync.service'
 chroot /tmp/arm64-rootfs /bin/bash -c 'systemctl enable systemd-networkd'
 
